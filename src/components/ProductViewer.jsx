ecc6d63ed56dda55e66ed6fb1712fa2bcacbd6ed
Enoch <oyedejienoch@gmail.com>
2025-10-16 14:05:22 +0100

    3d product-viewer

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import useMacbookStore from '../store'
+import clsx from 'clsx';
+import { Canvas } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
+import MackbookModel14 from './models/Macbook-14';
+import StudioLights from './StudioLights';
+import ModelSwitcher from './three/ModelSwitcher';
+import { useMediaQuery } from 'react-responsive';
+
+const ProductViewer = () => {
+
+    const {color, scale, setColor, setScale} =useMacbookStore();
+
+    const isMobile =useMediaQuery({query: '(max-width: 1024px)'})
+
+  return (
+    <section id='product-viewer'>
+        <h2>Take a closer look</h2>
+
+        <div className='controls'>
+            <p className='info'>MacbookPro 16 in {color} / Space Black</p>
+
+            <div className='flex-center gap-5 mt-5'>
+                <div className='color-control'>
+                    <div onClick={()=>setColor('#adb5db')} className={clsx('bg-neutral-300', color === '#adb5db' && 'active')} />
+                    <div onClick={()=>setColor('#2e2e2c')} className={clsx('bg-neutral-900', color === '#2e2e2c' && 'active')} />
+                    
+                </div>
+
+                <div className='size-control'>
+                    <div onClick={()=>setScale(0.06)} className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')} >
+                        <p>14"</p>
+                    </div>
+                    <div onClick={()=>setScale(0.08)} className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')} >
+                        <p>16"</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <Canvas id='canvas' camera={{position: [0, 2, 5], fov: 50, near:0.1, far:100}}>
+            <StudioLights />
+            {/* <MackbookModel14 scale={0.06} color={color} position={[0, 0, 0]}/> */}
+            {/* <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 3} minPolarAngle={Math.PI / 3} /> */}
+            <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}/>
+        </Canvas>
+    </section>
+  )
+}
+
+export default ProductViewer
